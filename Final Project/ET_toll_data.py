4d76a65127960ba5463c58c571c7fed73e7b81db
Musharraf Abdullah <mushabd.22@gmail.com>
2025-08-23 19:17:45 +0530

    DAG successfully run on local machine, using airflow on docker and wsl-ubuntu

--- Final Project/ET_toll_data.py
+++ Final Project/ET_toll_data.py
@@ -2,6 +2,7 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
+import os
 
 
 # default arguments
@@ -27,39 +28,47 @@ dag = DAG(
 
 # create tasks
 
+project_dir = '/opt/airflow/project/' # NEW CONTAINER PATH'
+
+# download_data = BashOperator(
+#     task_id='download_data',
+#     bash_command=f'sudo curl https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/tolldata.tgz -o {project_dir}/staging_area/tolldata.tgz',
+#     dag=dag
+# )
+
 unzip_data = BashOperator(
     task_id = 'unzip_data',
-    bash_command = 'tar -xzf /home/project/airflow/dags/finalassignment/tolldata.tgz -C ./',
+    bash_command = f'tar -xzf {project_dir}/staging_area/tolldata.tgz -C {project_dir}/staging_area/',
     dag = dag
 )
 
 extract_data_from_csv = BashOperator(
     task_id='extract_data_from_csv',
-    bash_command="cut -d',' -f1,2,3,4 vehicle-data.csv | tr -d '\r' > csv_data.csv",
+    bash_command= f"cut -d',' -f1,2,3,4 '{project_dir}/staging_area/vehicle-data.csv' | tr -d '\r' > {project_dir}/staging_area/csv_data.csv",
     dag=dag,
 )
 
 extract_data_from_tsv = BashOperator(
     task_id='extract_data_from_tsv',
-    bash_command="cut -d$'\t' -f5,6,7 tollplaza-data.tsv | tr '\t' ',' | tr -d '\r' > tsv_data.csv",
+    bash_command= f"cut -d$'\t' -f5,6,7 {project_dir}/staging_area/tollplaza-data.tsv | tr '\t' ',' | tr -d '\r' > {project_dir}/staging_area/tsv_data.csv",
     dag=dag,
 )
 
 extract_data_from_fixed_width = BashOperator(
     task_id='extract_data_from_fixed_width',
-    bash_command="cut -c 59-61,62-67 payment-data.txt | tr ' ' ',' | tr -d '\r' > fixed_width_data.csv",
+    bash_command= f"cut -c 59-61,62-67 {project_dir}/staging_area/payment-data.txt | tr ' ' ',' | tr -d '\r' > {project_dir}/staging_area/fixed_width_data.csv",
     dag=dag,
 )
 
 consolidate_data = BashOperator(
     task_id='consolidate_data',
-    bash_command="paste -d',' csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv",
+    bash_command= f"paste -d',' {project_dir}/staging_area/csv_data.csv {project_dir}/staging_area/tsv_data.csv {project_dir}/staging_area/fixed_width_data.csv > {project_dir}/staging_area/extracted_data.csv",
     dag=dag,
 )
 
 transform_data = BashOperator(
     task_id = 'transform_data',
-    bash_command = "cut -f4 < extracted_data.csv | tr [a-z] [A-Z] > transformed_data.csv",
+    bash_command = f"cut -f4 {project_dir}/staging_area/extracted_data.csv | tr [a-z] [A-Z] > {project_dir}/transformed_data/transformed_data.csv",
     dag=dag,
 )
 
